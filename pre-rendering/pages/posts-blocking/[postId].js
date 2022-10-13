c8ba0791d3e63fb3609a2018fd66a6deb70923a6
fernandovissentini <fvissentini@gmail.com>
2022-10-13 12:27:59 -0300

    fallback examples

---
+++ pre-rendering/pages/posts-blocking/[postId].js
@@ -0,0 +1,61 @@
+import {useRouter} from "next/router";
+
+function PostDetail({post}) {
+
+  const router = useRouter()
+  if(router.isFallback) {
+    return <h1>Loading...</h1>
+  }
+
+  return <>
+    <h1>Post Details for {post.title}</h1>
+    <h2>{post.id}</h2>
+    <h2>{post.body}</h2>
+  </>
+}
+
+export async function getStaticPaths() {
+
+  // const resp = await fetch('https://jsonplaceholder.typicode.com/posts')
+  // const postsData = await resp.json()
+  // const paths = postsData.map(post => {
+  //   return {
+  //     params: { postId: `${post.id}` }
+  //   }
+  // })
+  return {
+    paths: [
+      {
+        params: { postId: '1' }
+      },
+      {
+        params: { postId: '2' }
+      },
+      {
+        params: { postId: '3' }
+      }
+    ],
+    // paths,
+    fallback: true
+  }
+}
+
+export async function getStaticProps(context) {
+  const {params} = context
+  const resp = await fetch(`https://jsonplaceholder.typicode.com/posts/${params.postId}`)
+  const postData = await resp.json()
+
+  if(!postData.id) {
+    return {
+      notFound: true
+    }
+  }
+
+  return {
+    props: {
+      post: postData
+    }
+  }
+}
+
+export default PostDetail
---
+++ pre-rendering/pages/posts-fallback/index.js
@@ -0,0 +1,31 @@
+import Link from "next/link";
+
+function PostList({posts}) {
+  return <>
+    <h1>List of Posts</h1>
+    {
+      posts.map(post => {
+        return (
+            <div key={post.id}>
+              <Link href={`posts-blocking/${post.id}`} passHref>
+                <h2>{post.id} {post.title}</h2>
+              </Link>
+              <hr/>
+            </div>
+        )
+      })
+    }
+  </>
+}
+
+export async function getStaticProps() {
+  const resp = await fetch('https://jsonplaceholder.typicode.com/posts')
+  const postsData = await resp.json()
+  return {
+    props: {
+      posts: postsData
+    }
+  }
+}
+
+export default PostList
