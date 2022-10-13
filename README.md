fa74645b6060ab8ee3a4702a752b2ae7385c0138
fernandovissentini <fvissentini@gmail.com>
2022-10-13 09:11:47 -0300

    add link to users page

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # nextjs-tutorial
+
+The code in this repo is for the tutorial [Next.js Tutorial for beginners](https://www.youtube.com/watch?v=9P8mASSREYM&list=PLC3y8-rFHvwgC9mj0qv972IO5DmD-H0ZH)
--- pre-rendering/pages/index.js
+++ pre-rendering/pages/index.js
@@ -1,3 +1,10 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <h1>Next.js pre-rendering</h1>
+  return <>
+    <h1>Next.js pre-rendering</h1>
+    <Link href={`/users`}>
+      <a>Users</a>
+    </Link>
+  </>
 }
