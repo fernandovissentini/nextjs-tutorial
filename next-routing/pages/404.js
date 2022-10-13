419a4ae1ac701cd3dd87ca112921f7a63a2ff72b
fernandovissentini <fvissentini@gmail.com>
2022-10-13 06:35:18 -0300

    add hello world and routing examples

---
+++ next-routing/pages/404.js
@@ -0,0 +1,5 @@
+function PageNotFound() {
+  return <h1>404 - This page was not found.</h1>
+}
+
+export default PageNotFound
---
+++ next-routing/pages/blog/index.js
@@ -0,0 +1,5 @@
+function Index() {
+  return <h1>Blog Root Page</h1>
+}
+
+export default Index
