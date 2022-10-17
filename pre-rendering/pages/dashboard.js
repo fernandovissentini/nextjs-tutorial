8d23e2086d89ca27c2b85edfd41a7cd9e3d57b38
fernandovissentini <fvissentini@gmail.com>
2022-10-17 11:32:38 -0300

    Add ServerSideRendering and client request data

---
+++ pre-rendering/pages/dashboard.js
@@ -0,0 +1,33 @@
+import {useState, useEffect} from "react";
+
+function Dashboard() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [dashboardData, setDashboardData] = useState(null );
+
+  useEffect(() => {
+    async function fetchDashboardData() {
+      const response = await fetch('http://localhost:4000/dashboard')
+      const dashboardData = await response.json()
+      setDashboardData(dashboardData)
+      setIsLoading(false)
+    }
+    fetchDashboardData()
+  }, [])
+
+  if(isLoading) {
+    return <h2>Loading...</h2>
+  }
+
+  return (
+      <div>
+        <h2>Dashboard</h2>
+        <h3>Posts - {dashboardData.posts}</h3>
+        <h3>Likes - {dashboardData.likes}</h3>
+        <h3>Followers - {dashboardData.followers}</h3>
+        <h3>F ollowing - {dashboardData.following}</h3>
+      </div>
+  )
+
+}
+
+export default Dashboard
