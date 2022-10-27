d17fc0bb95f507c1ef4cc76e5f1ac914d66ec14f
fernandovissentini <fvissentini@gmail.com>
2022-10-27 12:28:01 -0300

    add app layout

---
+++ next-misc/pages/_app.js
@@ -0,0 +1,21 @@
+import '../styles/globals.css'
+import '../styles/layout.css'
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+function MyApp({Component, pageProps}) {
+
+  if(Component.getLayout) {
+    return Component.getLayout(<Component {...pageProps} />)
+  }
+
+  return (
+      <>
+        <Header />
+        <Component {...pageProps} />
+        <Footer />
+      </>
+  )
+}
+
+export default MyApp
