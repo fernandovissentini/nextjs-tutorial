b9dd878793d7a1076ed60691953af7119343de59
fernandovissentini <fvissentini@gmail.com>
2022-10-27 11:09:46 -0300

    add component level styles

--- next-styling/pages/about.js
+++ next-styling/pages/about.js
@@ -1,7 +1,10 @@
+import styles from '../styles/About.module.css'
+
 function About() {
   return (
-      <div>
-        <h2>About Page</h2>
+      <div className={styles.highlight}>
+        <h1>About Page</h1>
+        <h2>About Page Subtitle</h2>
         <button className='btn btn-primary'>Primary</button>
         <button className='btn btn-success'>Success</button>
       </div>
--- next-styling/pages/contact.js
+++ next-styling/pages/contact.js
@@ -1,5 +1,12 @@
+import styles from '../styles/Contact.module.css'
+
 function Contact() {
-  return <h2>Contact Page</h2>
+  return (
+      <div className={styles.highlight}>
+        <h1>Contact Page</h1>
+        <h2>Contact Page subtitle</h2>
+      </div>
+  )
 }
 
 export default Contact
