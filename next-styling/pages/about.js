fa968be86c49d3e28d7402872291ee5a0962f0c1
fernandovissentini <fvissentini@gmail.com>
2022-10-27 11:16:11 -0300

    add SASS support

--- next-styling/pages/about.js
+++ next-styling/pages/about.js
@@ -1,8 +1,8 @@
-import styles from '../styles/About.module.css'
+import styles from '../styles/About.module.scss'
 
 function About() {
   return (
-      <div className={styles.highlight}>
+      <div className={styles.highlightscss}>
         <h1>About Page</h1>
         <h2>About Page Subtitle</h2>
         <button className='btn btn-primary'>Primary</button>
--- next-styling/pages/contact.js
+++ next-styling/pages/contact.js
@@ -1,8 +1,8 @@
-import styles from '../styles/Contact.module.css'
+import styles from '../styles/Contact.module.scss'
 
 function Contact() {
   return (
-      <div className={styles.highlight}>
+      <div className={styles.highlightscss}>
         <h1>Contact Page</h1>
         <h2>Contact Page subtitle</h2>
       </div>
