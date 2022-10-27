a36458f7f741926cca43516f82b0f7242c725de3
fernandovissentini <fvissentini@gmail.com>
2022-10-27 11:00:35 -0300

    Add new project for styling add global styles in _app.js

---
+++ next-styling/pages/about.js
@@ -0,0 +1,11 @@
+function About() {
+  return (
+      <div>
+        <h2>About Page</h2>
+        <button className='btn btn-primary'>Primary</button>
+        <button className='btn btn-success'>Success</button>
+      </div>
+  )
+}
+
+export default About
