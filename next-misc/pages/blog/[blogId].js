be48a5bbb593062d597edbb4d90d46f0fda9b2e2
fernandovissentini <fvissentini@gmail.com>
2022-10-28 12:26:54 -0300

    add environment variables

--- next-misc/pages/blog/[blogId].js
+++ next-misc/pages/blog/[blogId].js
@@ -8,6 +8,7 @@ function Blog({title, description}) {
           <description name='description' content={description}/>
         </Head>
         <h1 className='content'>Article</h1>
+        <h1 className='content'>Env: {process.env.NEXT_PUBLIC_ANALYTICS_ID}</h1>
       </>
   )
 }
@@ -15,6 +16,11 @@ function Blog({title, description}) {
 export default Blog
 
 export async function getServerSideProps() {
+  const user = process.env.DB_USER
+  const password = process.env.DB_PASSWORD
+
+  console.log(user, password)
+
   return {
     props: {
       title: 'Article Title',
