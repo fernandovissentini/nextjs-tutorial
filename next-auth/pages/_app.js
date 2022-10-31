ec1bd8a489d20d6af62b4782b6176f8622e48ff9
fernandovissentini <fvissentini@gmail.com>
2022-10-31 10:19:36 -0300

    add new project for authentication

---
+++ next-auth/pages/_app.js
@@ -0,0 +1,14 @@
+import '../styles/globals.css'
+import Navbar from '../components/Navbar'
+import '../components/Navbar.css'
+
+function MyApp({ Component, pageProps }) {
+  return (
+      <>
+        <Navbar />
+        <Component {...pageProps} />
+      </>
+  )
+}
+
+export default MyApp
