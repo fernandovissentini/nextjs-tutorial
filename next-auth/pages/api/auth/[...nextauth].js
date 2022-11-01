72bde37b7fa9158a023b767e68749c3fb6c94f0f
fernandovissentini <fvissentini@gmail.com>
2022-11-01 14:17:09 -0300

    Connecting to mongodb database

--- next-auth/pages/api/auth/[...nextauth].js
+++ next-auth/pages/api/auth/[...nextauth].js
@@ -1,5 +1,7 @@
 import NextAuth from 'next-auth'
 import GithubProvider from 'next-auth/providers/github'
+import clientPromise from "../../../lib/mongodb"
+import {MongoDBAdapter} from "@next-auth/mongodb-adapter";
 
 export default NextAuth({
   providers: [
@@ -7,6 +9,13 @@ export default NextAuth({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET
     })
-  ]
+  ],
+  adapter: MongoDBAdapter(clientPromise),
+  session: {
+    jwt: true
+  },
+  jwt: {
+    secret: 'RandomS tring'
+  }
 })
 
