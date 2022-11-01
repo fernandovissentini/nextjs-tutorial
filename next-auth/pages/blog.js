08511997506a03acee0791353543702de9f4b496
fernandovissentini <fvissentini@gmail.com>
2022-11-01 13:19:52 -0300

    Securing pages server-side

--- next-auth/pages/blog.js
+++ next-auth/pages/blog.js
@@ -16,6 +16,16 @@ export default Blog
 export async function getServerSideProps(context) {
   const session = await getSession(context)
   console.log(session)
+
+  if(!session) {
+    return {
+      redirect: {
+        destination: `/api/auth/signin?callbackUrl=http://localhost:3000/blog`,
+        permanent: false
+      }
+    }
+  }
+
   return {
     props: {
       session,
