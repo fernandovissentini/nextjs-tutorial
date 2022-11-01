74aac74835fdab1671ec0ac0b360f083e42b2c26
fernandovissentini <fvissentini@gmail.com>
2022-11-01 13:15:12 -0300

    add Server-side authentication

--- next-auth/pages/blog.js
+++ next-auth/pages/blog.js
@@ -1,5 +1,25 @@
-function Blog() {
-  return <h1>Blog page</h1>
+import {getSession, useSession} from "next-auth/react";
+
+function Blog({data}) {
+  const {data: session, status} = useSession()
+  console.log(`Blog ${session} ${status}`)
+  return (
+      <>
+        <h1>Blog page</h1>
+        <h2>{data}</h2>
+      </>
+  )
 }
 
 export default Blog
+
+export async function getServerSideProps(context) {
+  const session = await getSession(context)
+  console.log(session)
+  return {
+    props: {
+      session,
+      data: session ? 'Authenticated User' : 'Non Authenticated user'
+    }
+  }
+}
